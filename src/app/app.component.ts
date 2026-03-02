6d2afab78c97db92dcf3b1c69ccc48ca764d8f5f
pardoma <pardoma@uji.es>
2026-03-02 14:08:13 +0100

    chore: First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,110 @@
+import {Component} from '@angular/core';
+import { HeaderComponent, FooterComponent } from '@eo4geo/ngx-bok-utils';
+import { MenuItem } from 'primeng/api';
+import { Router, RouterOutlet } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+})
+export class AppComponent {
+  headerItems: MenuItem[] = [ 
+    {
+      label: 'Tools',
+      icon: 'pi pi-cog',
+      items: [
+        {
+          label: 'BoK Visualization & Search',
+          icon: 'pi pi-search',
+          url: 'https://geospacebok.eu/',
+          target: '_blank'
+        },
+        {
+          label: 'Occupational Profile Tool',
+          icon: 'pi pi-users',
+          url: 'https://eo4geo-opt.web.app',
+          target: '_blank'
+        },
+        {
+          label: 'Job Offer Tool',
+          icon: 'pi pi-book',
+          url: 'https://eo4geo-jot.web.app',
+          target: '_blank'
+        },
+        {
+          label: 'Curriculum Design Tool',
+          icon: 'pi pi-sitemap',
+          style: {'--p-tieredmenu-item-color': 'var(--hover-color)'},
+          iconStyle: {'color': 'var(--hover-color)'}
+        },
+        {
+          label: 'Training Catalogue Tool',
+          icon: 'pi pi-graduation-cap',
+          url: 'https://spacesuite-project-tct.web.app/',
+          target: '_blank'
+        },
+        {
+          label: 'Skill Portfolio',
+          icon: 'pi pi-user',
+          url: 'https://spacesuite-project-spt.web.app',
+          target: '_blank'
+        },
+        {
+          label: 'BoK Annotation Tool',
+          icon: 'pi pi-pencil',
+          url: 'https://eo4geo-bat.web.app',
+          target: '_blank'
+        },
+        {
+          label: 'BoK Matching Tool',
+          icon: 'pi pi-equals',
+          url: 'https://eo4geo-bmt.web.app',
+          target: '_blank'
+        }
+      ]
+    }, 
+    {
+      label: 'Share',
+      icon: 'pi pi-share-alt',
+      items: [
+        {
+          label: 'X',
+          icon: 'pi pi-twitter',
+          url: 'https://twitter.com/SpaceSUITE_eu',
+          target: '_blank'
+        },
+        {
+          label: 'Facebook',
+          icon: 'pi pi-facebook',
+          url: 'https://www.facebook.com/spacesuiteproject/',
+          target: '_blank'
+        },
+        {
+          label: 'Youtube',
+          icon: 'pi pi-youtube',
+          url: 'https://www.youtube.com/@SpaceSUITE_eu',
+          target: '_blank'
+        },
+        {
+          label: 'LinkedIn',
+          icon: 'pi pi-linkedin',
+          url: 'https://www.linkedin.com/showcase/spacesuite_eu/',
+          target: '_blank'
+        }
+      ]
+    }
+  ];
+
+  constructor(private router: Router) {}
+
+  redirectToProfile() {
+    this.router.navigate(['profile'], { replaceUrl: true });
+  }
+
+  redirectToOrganizations() {
+    this.router.navigate(['organizations'], { replaceUrl: true });
+  }
+}
---
+++ src/app/components/mainPage/mainPage.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'main-page',
+  templateUrl: './mainPage.component.html',
+  styleUrls: ['./mainPage.component.css'],
+  imports: [],
+})
+export class MainPageComponent {
+
+}
