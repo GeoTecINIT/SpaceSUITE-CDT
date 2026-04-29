174af8b2462f8f9b8533925f130e54b5e9c29719
pardoma <pardoma@uji.es>
2026-04-29 12:53:23 +0200

    fix: Imporve Offer index; Show selected node description; fix index column css

--- src/app/components/offerIndexComponent/offerIndexComponent.component.ts
+++ src/app/components/offerIndexComponent/offerIndexComponent.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output, signal, SimpleChanges, WritableSignal} from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MenuItem, TreeNode } from "primeng/api";
+import { TreeNode } from "primeng/api";
 import { EducationalOffer } from "../../model/coreModel/educationalOffer";
 import { CurriculumNode } from "../../model/coreModel/curriculumNode";
 import { PanelModule } from "primeng/panel";
 import { TabsModule } from 'primeng/tabs';
 import { OrganizationChartModule } from 'primeng/organizationchart';
+import { TreeModule } from 'primeng/tree';
 import { SliderModule } from 'primeng/slider';
 import { FormsModule } from "@angular/forms";
 
@@ -14,14 +15,12 @@ import { FormsModule } from "@angular/forms";
   selector: 'offer-index-component',
   templateUrl: './offerIndexComponent.component.html',
   styleUrls: ['./offerIndexComponent.component.css'],
-  imports: [CommonModule, PanelModule, TabsModule, OrganizationChartModule, SliderModule, FormsModule],
+  imports: [CommonModule, PanelModule, TabsModule, OrganizationChartModule, SliderModule, FormsModule, TreeModule],
 })
 export class OfferIndexComponent {
   @Input() offer!: EducationalOffer;
   @Input() selectedNode: string = "";
   @Output() selectedNodeChanged: EventEmitter<string> = new EventEmitter();
-  selectedMenuItemId: WritableSignal<string> = signal<string>("");
-  menuItemsRoot: WritableSignal<MenuItem | undefined> = signal<MenuItem | undefined>(undefined);
   treeNodeRoot: WritableSignal<TreeNode[]> = signal<TreeNode[]>([]);
   selectedTreeNode: WritableSignal<TreeNode | undefined> = signal<TreeNode | undefined>(undefined);
 
@@ -32,9 +31,10 @@ export class OfferIndexComponent {
     this.selectedNodeChanged.emit(nodeId);
   }
 
-  onTreeNodeSelection(node: TreeNode) {
+  onTreeNodeSelection(node: TreeNode | TreeNode[] |  null) {
     if (!node) return;
-    this.selectedNodeChanged.emit(node.key!);
+    if (Array.isArray(node)) this.selectedNodeChanged.emit(node[0].key!);
+    else this.selectedNodeChanged.emit(node.key!);
   }
 
   preventTreeNodeUnselect(event: { node: TreeNode }) {
@@ -44,25 +44,14 @@ export class OfferIndexComponent {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['offer'] && changes['offer'].currentValue?.root !== changes['offer'].previousValue?.root) {
-      this.menuItemsRoot.set(this.buildMenuItem(changes['offer'].currentValue.root));
       this.treeNodeRoot.set(this.buildTreeNode(changes['offer'].currentValue.root));
     }
 
     if (changes['selectedNode'] && changes['selectedNode'].currentValue !== changes['selectedNode'].previousValue) {
-      this.selectedMenuItemId.set(changes['selectedNode'].currentValue);
       this.selectedTreeNode.set(this.getTreeNodeById(changes['selectedNode'].currentValue));
     }
   }
-  
-  private buildMenuItem(node: CurriculumNode): MenuItem {
-    const formattedName = this.getFormattedName(node.constructor.name);
-    return ({
-      id: node.id,
-      label: node.name,
-      items: node.getChildren().map(child => this.buildMenuItem(child)),
-      data: formattedName
-    });
-  }
+
 
   private buildTreeNode(node: CurriculumNode): TreeNode[] {
     const children = node.getChildren().map(child => this.buildTreeNode(child)).flat();
