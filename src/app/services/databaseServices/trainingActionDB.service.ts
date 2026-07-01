06cba22656d15f518493f110af112e754ebb08f0
pardoma <pardoma@uji.es>
2026-07-01 10:00:58 +0200

    fix: Concept parsing creating actions

--- src/app/services/databaseServices/trainingActionDB.service.ts
+++ src/app/services/databaseServices/trainingActionDB.service.ts
@@ -1,14 +1,16 @@
 import { inject, Injectable } from "@angular/core";
 import { collection, CollectionReference, doc, Firestore, serverTimestamp, setDoc } from "@angular/fire/firestore";
-import { map, Observable, of } from "rxjs";
+import { forkJoin, map, Observable, of, switchMap, take, tap } from "rxjs";
 import { TrainingAction } from "../../model/trainingActionModel/trainingAction";
 import { EducationalOffer } from "../../model/coreModel/educationalOffer";
 import { WorkloadUnit } from "../../model/trainingActionModel/trainingItem";
+import { BokInformationService } from "@eo4geo/ngx-bok-visualization";
 
 @Injectable({
     providedIn: 'root',
 })
 export class TrainingActionDBService {
+  private bokInformationService: BokInformationService = inject(BokInformationService);
   private db: Firestore = inject(Firestore);
   private actionCollection: CollectionReference;
 
@@ -17,8 +19,9 @@ export class TrainingActionDBService {
   }
 
   public createActionFromOffer(offer: EducationalOffer): Observable<string> {
-    const newAction: TrainingAction = this.OfferToActionAdapter(offer);
-    return this.setTrainingAction(newAction);
+    return this.OfferToActionAdapter(offer).pipe(
+      switchMap(newAction => this.setTrainingAction(newAction))
+    );  
   }
 
   private setTrainingAction(newAction: TrainingAction): Observable<string> {
@@ -30,14 +33,16 @@ export class TrainingActionDBService {
     return of(setDoc(newDocRef, newAction.toPlain())).pipe(map(() => newAction._id));
   }
 
-  private OfferToActionAdapter(offer: EducationalOffer): TrainingAction {
+  private OfferToActionAdapter(offer: EducationalOffer): Observable<TrainingAction> {
     const subjects: string[] = [];
     const concepts: string[] = [];
+
     offer.root.bokConcepts.forEach(value => {
       if (value.length == 2 || value == 'GIST') subjects.push(value);
       else concepts.push(value);
     });
-    return new TrainingAction(
+
+    const newTrainingAction = new TrainingAction(
       {
         title: offer.root.name,
         subject: subjects,
@@ -56,5 +61,22 @@ export class TrainingActionDBService {
         //isPublic: offer.isPublic,
       }
     );
+
+    return this.formatConceptsToFirestore(newTrainingAction.concepts).pipe(
+      tap(formattedConcepts => {
+        newTrainingAction.concepts = formattedConcepts;
+      }),
+      map(() => newTrainingAction)
+    );
+  }
+
+  private formatConceptsToFirestore(concepts: string[]): Observable<string[]> {
+    if (concepts.length <= 0) return of([]);
+    return forkJoin(concepts.map(concept =>
+      this.bokInformationService.getConceptName(concept).pipe(
+        take(1),
+        map(conceptName => `[${concept}] ${conceptName}`)
+      )
+    ))
   }
 }
